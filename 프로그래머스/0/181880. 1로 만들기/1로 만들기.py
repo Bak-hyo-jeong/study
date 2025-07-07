1fb342d4715ceedfc72e0e0b901aa69f7e08c58a
Bak-hyo-jeong <112619685+Bak-hyo-jeong@users.noreply.github.com>
2025-07-07 23:41:07 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -3,12 +3,7 @@ def solution(num_list):
     
     for i in num_list:
         while i != 1:
-            if i % 2 == 0:
-                i = i // 2
-                answer += 1
-            else:
-                i = i - 1
-                i = i // 2
-                answer += 1      
+            i = i // 2
+            answer += 1      
     
     return answer
