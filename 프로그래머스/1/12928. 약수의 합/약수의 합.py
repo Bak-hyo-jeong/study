65e21f9390ec19561ab52d6349dd3c4a5025cded
Bak-hyo-jeong <112619685+Bak-hyo-jeong@users.noreply.github.com>
2025-07-09 16:34:35 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,15 @@
 def solution(n):
-    answer = 0
+  #  answer = 0
+    sn = int(n ** 0.5)
+    aa = []
+    aa = set()
+    # for i in range(1, n + 1):
+    #     if n % i == 0:
+    #         answer += i              
     
-    for i in range(1, n + 1):
+    for i in range(1, sn+1):
         if n % i == 0:
-            answer += i        
-        
-    return answer
+            aa.add(i)
+            aa.add(n // i)
+         
+    return sum(aa)
