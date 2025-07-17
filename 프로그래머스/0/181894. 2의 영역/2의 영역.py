87bd94d59035cca64743f158bdc769344d7cc8f7
Bak-hyo-jeong <112619685+Bak-hyo-jeong@users.noreply.github.com>
2025-07-17 20:37:13 +0900

    [level 0] Title: 2의 영역, Time: 6.38 ms, Memory: 14.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    answer = []
+    a = []
+    
+    for i in range(len(arr)):
+        if arr[i] == 2:
+            a.append(i)
+    
+    if a:
+        answer = arr[min(a):max(a)+1]
+    else:
+        return [-1]
+    
+    return answer if answer else [-1]
