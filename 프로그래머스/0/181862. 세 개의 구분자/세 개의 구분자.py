08e87e415ed447d211691032202cf420c02e5654
Bak-hyo-jeong <112619685+Bak-hyo-jeong@users.noreply.github.com>
2025-07-17 19:31:52 +0900

    [level 0] Title: 세 개의 구분자, Time: 66.99 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,20 +1,29 @@
 def solution(myStr):
     answer = []
-    dl = []
     tmp = ''
     
-    for i in myStr:
-        if i == "a" or i == "b" or i == "c":
-            if tmp != "" :
-                answer.append(tmp)
-                tmp = ""
-        else:
-            tmp += i
+#     for i in myStr:
+#         if i == "a" or i == "b" or i == "c":
+#             if tmp != "" :
+#                 answer.append(tmp)
+#                 tmp = ""
+#         else:
+#             tmp += i
             
-    if tmp != "":
-        answer.append(tmp)
+#     if tmp != "":
+#         answer.append(tmp)
         
-    if answer == []:
-        answer.append("EMPTY")
+#     if answer == []:
+#         answer.append("EMPTY")
     
-    return answer
+    for ch in myStr:
+        if ch in 'abc':
+            if tmp:
+                answer.append(tmp)
+                tmp = ''
+        else:
+            tmp += ch
+    if tmp:
+        answer.append(tmp)
+    
+    return answer if answer else ["EMPTY"]
