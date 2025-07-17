73c64601542d3f00815620fb54eee621c5b1d6b1
Bak-hyo-jeong <112619685+Bak-hyo-jeong@users.noreply.github.com>
2025-07-17 21:00:58 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 9.05 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -5,9 +5,7 @@ def solution(arr, queries):
     for i in queries:
         s, e, k = i
         for j in range(s, e+1, 1):
-            if j == 0:
-                arr[j] += 1
-            elif j % k == 0:
+            if j % k == 0:
                 arr[j] += 1
     
     return arr
