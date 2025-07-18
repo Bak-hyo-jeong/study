32856799352cb1e02636a4a89472ad45f1c1f1bd
Bak-hyo-jeong <112619685+Bak-hyo-jeong@users.noreply.github.com>
2025-07-19 08:07:12 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.06 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,15 +1,22 @@
+import numpy as np
+
 def solution(left, right):
     answer = 0
-    
+            
     for i in range(left, right+1):
-        o, e = 0, 0
-        for j in range(1, i+1):
-            if i % j == 0:
-                o += 1
-        if o % 2 == 0:
-            answer += i
-        else :
+        if i % (i ** 0.5) == 0:
             answer -= i
-        
-    
+        else :
+            answer += i
+            
     return answer
+
+    # for i in range(left, right+1):
+    #     o = 0
+    #     for j in range(1, i+1):
+    #         if i % j == 0:
+    #             o += 1
+    #     if o % 2 == 0:
+    #         answer += i
+    #     else :
+    #         answer -= i
